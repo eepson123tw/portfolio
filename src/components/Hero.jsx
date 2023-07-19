745c344255a23feb6c62c76070ff893262bfe18f
allen <eepson123@gmail.com>
2023-07-19 23:30:12 +0800

    feat(hero) :add hero canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,44 @@
+// @ts-nocheck
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
 export default function Hero() {
-  return <div>Hero</div>
+  return (
+    <section className='relative w-full mx-auto h-screen'>
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-red-100'></div>
+          <div className='w-1 sm:h-80 h-40 bg-gradient-to-b from-red-100  '></div>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className=' text-red-300'>Allen</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop this portfolio and <br className='sm:block hidden' />
+            some web applications.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-white mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,4 +1,21 @@
 import React from 'react'
+import { Html, useProgress } from '@react-three/drei'
 export default function Loader() {
-  return <div>Loader</div>
+  const { progress } = useProgress()
+  return (
+    <Html>
+      <span className='canvas-load'></span>
+      <p
+        style={{
+          fontSize: '5vw',
+          color: 'lightcoral',
+          textAlign: 'center',
+          fontWeight: 800,
+          marginTop: 40
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  )
 }
