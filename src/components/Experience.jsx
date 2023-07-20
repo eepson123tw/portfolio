33e3b70ab0f6a751de8208fc85db538bf0dedde2
allen <eepson123@gmail.com>
2023-07-20 22:57:12 +0800

    feat(experience): add the experience page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,67 @@
+// @ts-nocheck
 import React from 'react'
-export default function Experience() {
-  return <div>Experience</div>
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from 'react-vertical-timeline-component'
+import { motion } from 'framer-motion'
+import 'react-vertical-timeline-component/style.min.css'
+import { styles } from '../styles'
+import { experiences } from '../constants'
+import SectionWrapper from '../hoc'
+import { textVariant } from '../utils/motions'
+
+function Experience() {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Experience.</h2>
+      </motion.div>
+      <div className='mt-20 flex flex-col '>
+        <VerticalTimeline>
+          {experiences.map((ex, index) => (
+            <ExperienceCard key={index} {...ex} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  )
 }
+
+function ExperienceCard({ iconBg, date, title, company_name, points, icon }) {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{ background: '#1d1836', color: '#fff' }}
+      contentArrowStyle={{ borderRight: '7px solid #232631' }}
+      date={date}
+      iconStyle={{ background: iconBg }}
+      icon={
+        <div className='flex justify-center items-center w-full h-full'>
+          <img
+            className='w-[60%] h-[60%] object-contain'
+            src={icon}
+            alt={company_name}
+          />
+        </div>
+      }
+    >
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{title}</h3>
+        <p
+          className='text-secondary text-[16px] font-semibold'
+          style={{ margin: 0 }}
+        >
+          {company_name}
+        </p>
+      </div>
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {points.map((point, index) => (
+          <li key={index}>{point}</li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  )
+}
+
+export default SectionWrapper(Experience)
--- src/constants/index.js
+++ src/constants/index.js
@@ -116,56 +116,20 @@ const technologies = [
 
 const experiences = [
   {
-    title: 'React.js Developer',
-    company_name: 'Starbucks',
-    icon: starbucks,
+    title: 'Vue.js Developer',
+    company_name: 'Sheng Jin Integrated Services',
+    icon: 'https://www.sjis.com.tw/img/logo-sjis.png',
     iconBg: '#383E56',
-    date: 'March 2020 - April 2021',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.'
-    ]
-  },
-  {
-    title: 'React Native Developer',
-    company_name: 'Tesla',
-    icon: tesla,
-    iconBg: '#E6DEDD',
-    date: 'Jan 2021 - Feb 2022',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.'
-    ]
+    date: 'March 2021 - March 2022',
+    points: ['', '', '', '']
   },
   {
     title: 'Web Developer',
-    company_name: 'Shopify',
-    icon: shopify,
-    iconBg: '#383E56',
-    date: 'Jan 2022 - Jan 2023',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.'
-    ]
-  },
-  {
-    title: 'Full stack Developer',
-    company_name: 'Meta',
-    icon: meta,
+    company_name: 'Starlux Airlines',
+    icon: 'https://www.logo9.net/logoulchart/allimg/starlux-02.jpg',
     iconBg: '#E6DEDD',
-    date: 'Jan 2023 - Present',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.'
-    ]
+    date: 'April 2022 - Current',
+    points: ['', '', '', '']
   }
 ]
 
