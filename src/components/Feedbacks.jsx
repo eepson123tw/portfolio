7c5410dd633251b40fdab592a3a3f209ffbbec40
allen <eepson123@gmail.com>
2023-07-22 12:33:44 +0800

    feat(feedBack): add the feedBack page

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,4 +1,67 @@
+// @ts-nocheck
 import React from 'react'
-export default function Feedbacks() {
-  return <div>Feedbacks</div>
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import SectionWrapper from '../hoc'
+import { fadeIn, textVariant } from '../utils/motions'
+import { testimonials } from '../constants'
+function Feedbacks() {
+  return (
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div
+        className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7 justify-center md:justify-start`}
+      >
+        {testimonials.map((test, index) => (
+          <FeedbackCard key={index} {...test} />
+        ))}
+      </div>
+    </div>
+  )
 }
+
+function FeedbackCard({
+  testimonial,
+  name,
+  designation,
+  company,
+  image,
+  index
+}) {
+  return (
+    <motion.div
+      variants={fadeIn('', 'spring', 0.5 * index, 0.75)}
+      className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+    >
+      <p className='text-white font-black text-[48px]'>"</p>
+      <div className='mt-1'>
+        <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+        <div className='mt-7 flex justify-between items-center gap-1'>
+          <div className='flex-1 flex flex-col'>
+            <p className='text-white font-medium text-[16px]'>
+              <span className='blue-text-gradient'>@</span>
+              {name}
+            </p>
+            <p className='mt-1 text-secondary text-[12px]'>
+              {designation} of {company}
+            </p>
+          </div>
+          <img
+            src={image}
+            alt={`$feed-back-by-${name}`}
+            className='w-10 h-10 rounded-full object-cover'
+          />
+        </div>
+      </div>
+    </motion.div>
+  )
+}
+
+export default SectionWrapper(Feedbacks, '')
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Tilt from 'react-parallax-tilt'
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
-import { services } from '../constants'
 import SectionWrapper from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motions'
@@ -85,4 +84,4 @@ function ProjectCard({
   )
 }
 
-export default SectionWrapper(Works, '')
+export default SectionWrapper(Works, 'work')
